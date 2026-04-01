773bf560354f954c1509f70e1a956f987a0a5d97
Hunde <hundealex443@gmail.com>
2026-04-01 11:30:55 -0700

    implement the websocket server

--- source/routes/matches.js
+++ source/routes/matches.js
@@ -45,10 +45,16 @@ matchRouter.post('/',async (req,res)=>{
         awayScore:awayScore ?? 0,
         status:getMatchStatus(startTime,endTime),
        }).returning();
+        
+    if(res.app.locals.broadcastMatchCreated){
+        res.app.locals.broadcastMatchCreated(event);
+    }
+
        res.status(201).json({data:event});
     }catch(e){
         console.log(e);
         res.status(500).json({
             error:'Failed to create match.'});
     }
+
 })
